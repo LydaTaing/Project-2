914eab341b21bfde9e716aad35940a3b81fea071
Amshu Bellur <abell062@ucr.edu>
2025-05-27 21:46:06 -0700

    added the crossValidation function: calculates the accuracy in comparison to each point

--- NNValidator.py
+++ NNValidator.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import Counter
 
+#class that chooses the class for the point based on eucidean distance
 class NearestNeighborClassifier:
     #Initializes the classifier with a given k, default is k=1 for simplicity.
     def __init__(self, k=1):
@@ -40,3 +41,39 @@ class NearestNeighborClassifier:
         #count and return most common class label (majority vote)
         most_common = Counter(kNearestLabels).most_common(1)
         return most_common[0][0]
+    
+
+#goes through each point in the data set and then caluates the overall accuracy
+#arg: features (np.ndarray): The full dataset features
+#args: labels (np.ndarray): The full dataset labels
+#args: k_neighbors (int): The number of neighbors (k) for the KNN classifier
+#return: accuracy of classifier
+def crossValidation(features, labels, kNeighbors):
+    #count the correct ad total to make accuracy 
+    correctPredictions = 0
+    numInstances = features.shape[0]
+    
+    #loop through all the instances
+    for i in range(numInstances):
+        #the test data for this round
+        testInstance = features[i]
+        testLabel = labels[i]
+        
+        #all other instances are the training data
+        trainingInstances = np.delete(features, i, axis=0)
+        trainingLabels = np.delete(labels, i, axis=0)
+        
+        #initialize and train classifier
+        classifier = NearestNeighborClassifier(k=kNeighbors)
+        classifier.train(trainingInstances, trainingLabels)
+        
+        #prediction
+        prediction = classifier.test(testInstance)
+        
+        #is the prediction correct?
+        if prediction == testLabel:
+            correctPredictions += 1
+            
+    #calculate and return the accuracy
+    accuracy = correctPredictions / numInstances
+    return accuracy
